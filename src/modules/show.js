cdff3bf7bac275d79a939893c75eeebf6ffef05f
Jean Nunes <jean.nunes@uzzipay.com>
2024-10-08 16:32:01 -0400

    Show progress

--- src/modules/show.js
+++ src/modules/show.js
@@ -5,8 +5,13 @@ const cutCount = document.querySelector("#js-cut-count");
 const history = document.querySelector(".history-items");
 const cardId = document.querySelector(".card-id > span");
 const cutSlots = document.querySelectorAll(".slots-item");
+const cutsRemaining = document.querySelector(".counting-container > span");
+const cutsProgressText = document.querySelector(
+  ".counting-bar-container > span"
+);
+const progressIndicator = document.querySelector(".progress-indicator");
 
-function clearFields() {
+function clearInfo() {
   avatarImg.src = `src/assets/user.jpg`;
   clientName.textContent = "";
   membershipDate.textContent = "";
@@ -22,10 +27,13 @@ function clearFields() {
 }
 
 export const clientShow = (client) => {
-  clearFields();
+  //Limpar informações
+  clearInfo();
+  //Exibir dados do usuário
   avatarImg.src = `src/assets/${client.id}.jpg`;
   clientName.textContent = client.name;
   membershipDate.textContent = `Cliente desde ${client.clientSince}`;
+  //Exibir dados do histórico
   const cuts = client.loyaltyCard.totalCuts;
   cutCount.textContent = `${cuts} ${cuts > 0 ? "cortes" : "corte"}`;
   for (const appointment of client.appointmentHistory) {
@@ -47,11 +55,21 @@ export const clientShow = (client) => {
     historyItem.append(itemMoment);
     historyItem.append(itemCheck);
     history.append(historyItem);
-    cardId.textContent = client.id;
-    cutSlots.forEach((cutSlot, index) => {
-      if (index < cuts) {
-        cutSlot.innerHTML = '<img src="./src/assets/PinCheck.png" />';
-      }
-    });
   }
+  //Exibir dados do cartão fidelidade
+  cardId.textContent = client.id;
+  cutSlots.forEach((cutSlot, index) => {
+    if (index < cuts) {
+      cutSlot.innerHTML = '<img src="./src/assets/PinCheck.png" />';
+    }
+  });
+  //Exibir progresso
+  const cutsRemainingNumber = client.loyaltyCard.cutsRemaining;
+  cutsRemaining.textContent = `${cutsRemainingNumber} ${
+    cutsRemainingNumber > 1 ? "cortes restantes" : "corte restante"
+  }`;
+  cutsProgressText.textContent = `${client.loyaltyCard.totalCuts} de  ${client.loyaltyCard.cutsNeeded}`;
+  const progressPercentage =
+    (client.loyaltyCard.totalCuts * 100) / client.loyaltyCard.cutsNeeded;
+  progressIndicator.style.width = `${progressPercentage}%`;
 };
